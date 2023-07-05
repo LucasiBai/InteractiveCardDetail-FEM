73dfaa75c72a1ff932d992b8f6c81b0aa4146587
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-07-04 21:09:50 -0300

    correccion de errores de actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
 import { CardModule } from './modules/card/card.module';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent],
